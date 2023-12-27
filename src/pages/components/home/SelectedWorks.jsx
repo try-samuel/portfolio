5200d84fd20d1f8bdb4aa9fb19de770717cd1af6
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2023-12-27 22:18:35 +0100

    Works section starts

--- src/pages/components/home/SelectedWorks.jsx
+++ src/pages/components/home/SelectedWorks.jsx
@@ -1,6 +1,10 @@
 import WorkCard from "./WorkCard";
 import sardionCareImage from "../../../assets/images/sardion-care.png";
 import nerfedFlixImage from "../../../assets/images/nerfed-flix.png";
+import bookstoreImage from "../../../assets/images/bookstore.png";
+import todoAppImage from "../../../assets/images/todo-app.png";
+import quizAppImage from "../../../assets/images/quiz-app.png";
+import weatherAppImage from "../../../assets/images/weather-app.png";
 
 const SelectedWorks = () => {
   return (
@@ -8,8 +12,8 @@ const SelectedWorks = () => {
       <h1 className="text-xl mb-5 capitalize">selected works</h1>
       <div className="grid gap-10 xs:grid-cols-1 lg:grid-cols-2 xs:grid-rows-6 lg:grid-rows-3">
         <WorkCard
-          title="Sardion Care"
-          description="Website"
+          title={"Sardion Care"}
+          description={"Website"}
           link={"https://sardioncare.ca/"}
           image={sardionCareImage}
         />
@@ -18,6 +22,30 @@ const SelectedWorks = () => {
           description={"Web-App"}
           link={"https://nerfed-flix-s3db.vercel.app/"}
           image={nerfedFlixImage}
+        />
+        <WorkCard
+          title={"Book-Store"}
+          description={"Web-App"}
+          link={"https://bookstore-frontend-8b7pxtybn-weird-samuel.vercel.app/"}
+          image={bookstoreImage}
+        />
+        <WorkCard
+          title={"Todo-App"}
+          description={"Web-App"}
+          link={"https://to-do-app-59eb.onrender.com/"}
+          image={todoAppImage}
+        />
+        <WorkCard
+          title={"Quiz-App"}
+          description={"Mini-Web-App"}
+          link={"https://weird-samuel.github.io/Quiz-App/"}
+          image={quizAppImage}
+        />
+        <WorkCard
+          title={"Weather-App"}
+          description={"Mini-Web-App"}
+          link={"https://weird-samuel.github.io/Weather-app/"}
+          image={weatherAppImage}
         />
       </div>
     </section>

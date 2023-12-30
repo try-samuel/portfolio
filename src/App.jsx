b38a1f22df5df4e5261b4e051e9d485334e163c5
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2023-12-30 20:09:32 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ErrorPage from "./pages/ErrorPage";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 

010a0d91361e679d8c9d2d35e60614d3ff39027d
Adekunle Samuel Fiyinfoluwa <110774881+weird-samuel@users.noreply.github.com>
2024-05-02 09:06:24 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container align-center justify-center text-white py-12 px-5 select-none">
+    <div className="text-white py-12 px-5 select-none">
       {loading ? (
         <Loader timeout={2000} />
       ) : (

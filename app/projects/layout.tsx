6e6d095b7328eb058429324ce02c8f77a0103abd
try-samuel <fiyinfoluwasamuel580@gmail.com>
2025-03-03 17:17:27 +0100

    updatessssSS

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -5,9 +5,10 @@ export const metadata: Metadata = {
   description: "A collection of all my projects",
   openGraph: {
     title: "Adekunle Samuel Fiyinfoluwa",
-    description: "Sit back and explore all my projects for free",
+    description:
+      "This here is my portfolio You are lucky to see this because you get to see who I am in the tech space and explore all my projects for free",
     images:
-      "https://i.pinimg.com/564x/81/05/33/810533f3738b9189e7519f8064ff5dd7.jpg",
+      "https://i.pinimg.com/736x/b8/25/29/b825290411f0ca77c8c51e8440748da7.jpg",
   },
 };
 

d420fe233076e764cb753e9fbd53a360c30092de
try-samuel <fiyinfoluwasamuel580@gmail.com>
2025-03-18 06:38:14 +0100

    typos

--- lib/data.tsx
+++ lib/data.tsx
@@ -4,7 +4,7 @@ export const expereince = [
     company_name: "AfraB",
     period: "Jan 2025 - Present",
     description:
-      "Leding development teams across mobile web and backend initiatives, while collaborating on frontend architecture decisions and implementation. Worked closely with the design team to enhance user experience and implement responsive design principles. Drove performance optimization efforts and established quality standards through active code review participation. Managed team resource allocation and tooling setup, ensuring efficient development workflows.",
+      "Leading development teams across mobile web and backend initiatives, while collaborating on frontend architecture decisions and implementation. Worked closely with the design team to enhance user experience and implement responsive design principles. Drove performance optimization efforts and established quality standards through active code review participation. Managed team resource allocation and tooling setup, ensuring efficient development workflows.",
     location: "Remote, Nigeria",
   },
   {
